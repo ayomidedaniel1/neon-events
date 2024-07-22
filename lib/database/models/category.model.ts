8a9224b541424a6bc9efd32d530adb616534b585
Ayomide <ayomidedans@gmail.com>
2024-07-22 23:42:58 +0100

    feat: created order and category models

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,16 @@
+import { model } from "mongoose";
+import { models } from "mongoose";
+import { Document, Schema } from "mongoose";
+
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
+}
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, unique: true },
+});
+
+const Category = models.Category || model('Category', CategorySchema);
+
+export default Category;
