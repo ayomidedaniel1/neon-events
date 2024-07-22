674e2f844937cb7f0831c93aac4c5373f67ab1a3
Ayomide <ayomidedans@gmail.com>
2024-07-22 13:09:14 +0100

    init

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  '/(.*)',
+  '/events/:id(.*)',
+  '/api/webhook/clerk(.*)',
+  '/api/webhook/stripe(.*)',
+  '/api/uploadthing(.*)'
+]);
+
+// ignoredRoutes: [
+//   '/api/webhook/clerk',
+//   '/api/webhook/stripe',
+//   '/api/uploadthing'
+// ]
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
