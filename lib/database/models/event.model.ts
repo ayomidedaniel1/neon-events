5826473f27713090d4208f6475fc5a6b13b0ee13
Ayomide <ayomidedans@gmail.com>
2024-07-22 15:37:03 +0100

    feat: created events model

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,20 @@
-import { model, models, Schema } from "mongoose";
+import { Document, model, models, Schema } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string, name: string; };
+  organizer: { _id: string, firseName: string, lastName: string; };
+}
 
 const EventSchema = new Schema({
   title: { type: String, required: true },
