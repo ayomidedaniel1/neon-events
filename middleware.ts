3b703e5fa30a0caea231bdabb323f3dc35231e6e
Ayomide <ayomidedans@gmail.com>
2024-07-23 17:30:18 +0100

    chore: code cleanup

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// const isPublicRoute = createRouteMatcher([
-//   '/(.*)',
-//   '/events/:id(.*)',
-//   '/api/webhook/clerk(.*)',
-//   '/api/webhook/stripe(.*)',
-//   '/api/uploadthing(.*)'
-// ]);
-
-// ignoredRoutes: [
-//   '/api/webhook/clerk',
-//   '/api/webhook/stripe',
-//   '/api/uploadthing'
-// ]
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)",
-
-    // Skip Next.js internals and all static files, unless found in search params
-    // '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    // '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
