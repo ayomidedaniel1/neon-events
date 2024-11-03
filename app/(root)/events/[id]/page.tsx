a0d2ced8a3a1f0a7245f7ea02aa079c549e0a53b
Ayomide <ayomidedans@gmail.com>
2024-11-03 12:18:40 +0100

    feat: next config file change

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,9 +1,26 @@
+import { getEventById } from '@/lib/actions/event.actons';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image';
 import React from 'react';
 
-const page = () => {
+const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+  const event = await getEventById(id);
+
+  console.log(event);
+
   return (
-    <div>page</div>
+    <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
+      <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
+        <Image
+          src={event.imageUrl}
+          alt='hero image'
+          width={1000}
+          height={1000}
+          className='h-full min-h-[300px] object-cover object-center'
+        />
+      </div>
+    </section>
   );
 };
 
-export default page;
+export default EventDetails;
