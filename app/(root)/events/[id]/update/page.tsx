e51c6d1224916c04172b3e851b22b31cd3cc6b15
Ayomide <ayomidedans@gmail.com>
2024-11-04 09:52:31 +0100

    fix: bug with page alignment

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,8 +1,18 @@
 import EventForm from '@/components/shared/EventForm';
+import { getEventById } from '@/lib/actions/event.actons';
 import { auth } from '@clerk/nextjs/server';
 import React from 'react';
 
-const UpdateEvent = () => {
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+
+  const event = await getEventById(id);
+
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
@@ -14,7 +24,12 @@ const UpdateEvent = () => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type='Update' />
+        <EventForm
+          type='Update'
+          event={event}
+          eventId={event._id}
+          userId={userId}
+        />
       </div>
     </>
   );
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -20,16 +20,23 @@ import { useUploadThing } from "@/lib/uploadthing";
 
 import "react-datepicker/dist/react-datepicker.css";
 import { useRouter } from "next/navigation";
-import { createEvent } from "@/lib/actions/event.actons";
+import { createEvent, updateEvent } from "@/lib/actions/event.actons";
+import { IEvent } from "@/lib/database/models/event.model";
 
 type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
+  event?: IEvent;
+  eventId?: string;
 };
 
-const EventForm = ({ userId, type }: EventFormProps) => {
+const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
-  const initialValues = eventDefaultValues;
+  const initialValues = event && type === 'Update' ? {
+    ...event,
+    startDateTime: new Date(event.startDateTime),
+    endDateTime: new Date(event.endDateTime),
+  } : eventDefaultValues;
   const Router = useRouter();
 
   const { startUpload } = useUploadThing('imageUploader');
@@ -63,6 +70,28 @@ const EventForm = ({ userId, type }: EventFormProps) => {
         if (newEvent) {
           form.reset();
           Router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (type === 'Update') {
+      if (!eventId) {
+        Router.back();
+        return;
+      }
+
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImgUrl, _id: eventId },
+          path: `/events/${eventId}`
+        });
+
+        if (updatedEvent) {
+          form.reset();
+          Router.push(`/events/${updatedEvent._id}`);
         }
       } catch (error) {
         console.log(error);
